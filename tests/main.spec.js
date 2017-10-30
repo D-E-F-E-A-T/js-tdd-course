fac59b6ccbe8750dcbb81b4a5d3fe179da391728
=vmarcosp <=contato@vmarcosp.com.br>
2017-10-30 14:02:18 -0200

    Created simple calc module

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,25 +1,80 @@
-const expect = require('chai').expect
+import { expect } from 'chai'
+import { sum, sub, div, mult } from '../src/main.js'
 
-let arr = []
+describe('Calc', () => {
 
-beforeEach(() => {
-	arr = [1, 2, 3, 4]
+	//Smoke tests
+
+	describe('Smoke tests', () => {
+
+		it('should  exist the method sum', () => {
+			expect(sum).to.exist
+			expect(sum).to.be.a('function')
+		})
+
+		it('should  exist the method mult', () => {
+			expect(mult).to.exist
+			expect(mult).to.be.a('function')
+		})
+
+		it('should  exist the method sub', () => {
+			expect(sub).to.exist
+			expect(sub).to.be.a('function')
+		})
+
+		it('should  exist the method div', () => {
+			expect(div).to.exist
+			expect(div).to.be.a('function')
+		})
+
+		describe('Method: Sum', () => {
+
+			it('should return 4 when `sum(2,2)`', () => {
+				expect(sum(2, 2)).to.be.equal(4)
+			})
+
+			it('should return 10 when `sum(5,5)` ', () => {
+				expect(sum(5, 5)).to.be.equal(10)
+			})
+
+		})
+
+		describe('Method: Sub', () => {
+
+			it('should return 2 when `sum(4,2)`', () => {
+				expect(sub(4, 2)).to.be.equal(2)
+			})
+
+			it('should return -2 when `sub(2,4)`', () => {
+				expect(sub(2, 4)).to.be.equal(-2)
+			})
+
+		})
+
+		describe('Method: Mult', () => {
+
+			it('should return 4 when `mult(2,2)`', () => {
+				expect(mult(2, 2)).to.be.equal(4)
+			})
+
+			it('should return 10 when `mult(2,5)`', () => {
+				expect(mult(2, 5)).to.be.equal(10)
+			})
+
+		})
+
+		describe('Method: Div', () => {
+
+			it('should return 2 when `div(4,2)`', () => {
+				expect(div(4, 2)).to.be.equal(2)
+			})
+
+			it('should return `Division by 0 is invalid` when divide by 0', () => {
+				expect(div(4, 0)).to.be.equal('Division by 0 is invalid')
+			})
+
+		})
+
+	})
+
 })
-
-describe('Numbers', () => {
-
-	it('should be an array', () => {
-		expect(arr).to.be.an('array')
-	})
-
-	it('should have a size of 5 when push another value to the array', () => {
-		arr.push(5)
-		expect(arr).to.have.length(5)
-	})
-
-	it('should remove the value 4 when use pop method in array', () => {
-		arr.pop()
-		expect(arr).to.not.include(4)
-	})
-
-});
