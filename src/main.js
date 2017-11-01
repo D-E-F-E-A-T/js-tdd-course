3122723a6d2765dd4d1658533dc9739c45ff797c
Marcos Vinicius <contato@vmarcosp.com.br>
2017-11-01 00:57:38 -0200

    Added nyc and configured scripts :coffee:

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 const FizzBuzz = (number) => {
+  if (number === 0) return number;
   if (number % 5 === 0 && number % 3 === 0) return 'FizzBuzz';
   if (number % 3 === 0) return 'Fizz';
   if (number % 5 === 0) return 'Buzz';
   return number;
 };
+
+export const randomFunc = () => 'Random value :)';
 
 export default FizzBuzz;
